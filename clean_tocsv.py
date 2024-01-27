14bc4f9d4c5ad29e9c2d40bc775d8bbba2c6da08
unknown <2857054421@qq.com>
2024-01-27 15:03:04 +0800

    commit数据时间分析

---
+++ clean_tocsv.py
@@ -0,0 +1,81 @@
+import json
+import csv
+
+with open("data/issues.json", "r") as f:
+    data = json.load(f)
+
+with open("data/issues_all.csv", "w", newline="", encoding="utf-8") as csvfile:
+    csvwriter = csv.writer(csvfile)
+
+    # 提取的内容
+    header = [
+        "number",
+        "title",
+        "user",
+        "labels",
+        "state",
+        "locked",
+        "created_at",
+        "updated_at",
+        "closed_at",
+        "comments",
+    ]
+    csvwriter.writerow(header)
+
+    for item in data:
+        number = item["number"]
+        title = item["title"]
+        user = item["user"]["login"]
+        labels = [label["name"] for label in item["labels"]]
+        state = item["state"]
+        locked = item["locked"]
+        created_at = item["created_at"]
+        updated_at = item["updated_at"]
+        closed_at = item["closed_at"]
+        comments = item["comments"]
+
+        csvwriter.writerow(
+            [
+                number,
+                title,
+                user,
+                labels,
+                state,
+                locked,
+                created_at,
+                updated_at,
+                closed_at,
+                comments,
+            ]
+        )
+# commits原始数据的转换 todo
+
+with open("data/commits.json", "r") as f:
+    data = json.load(f)
+
+with open("data/commits_all.csv", "w", newline="", encoding="utf-8") as csvfile:
+    csvwriter = csv.writer(csvfile)
+
+    # 提取的内容
+    header = ["sha", "author", "date", "committer", "message", "comment_count"]
+
+    csvwriter.writerow(header)
+
+    for item in data:
+        sha = item["sha"]
+        author = item["commit"]["author"]["name"]
+        date = item["commit"]["author"]["date"]
+        committer = item["commit"]["committer"]["name"]
+        message = item["commit"]["message"]
+        comment_count = item["commit"]["comment_count"]
+
+        csvwriter.writerow(
+            [
+                sha,
+                author,
+                date,
+                committer,
+                message,
+                comment_count,
+            ]
+        )
---
+++ spider.py
@@ -0,0 +1,78 @@
+import requests
+import json
+
+
+# 获取部分信息
+def get_data(repo_owner, repo_name, data_type):
+    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/{data_type}"
+    headers = {
+        "Accept": "application/vnd.github.v3+json",
+        "Authorization": "token " + "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",
+    }
+    page = 1
+    per_page = 30
+    issues = []
+
+    while page <= 900:
+        params = {"state": "all", "page": page, "per_page": per_page}
+        response = requests.get(url, headers=headers, params=params)
+
+        if response.status_code == 200:
+            issues += response.json()
+            page += 1
+        else:
+            print(f"获取第 {page} 页issue失败")
+            break
+
+    return issues
+
+
+# 获取全部信息
+def get_all_data(repo_owner, repo_name, data_type):
+    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/{data_type}"
+    headers = {
+        "Accept": "application/vnd.github.v3+json",
+        "Authorization": "token " + "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",
+    }
+    issues = []
+    params = {"state": "all"}
+    # 发起GET请求获取第一页的信息
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        issues += response.json()
+
+        # 检查是否有更多的页码，如果有，继续请求下一页的信息
+        while "Link" in response.headers and 'rel="next"' in response.headers["Link"]:
+            for row in response.headers["Link"].split(", "):
+                if 'rel="next"' in row:
+                    next_page_url = row.split("; ")[0].strip("<>")
+            response = requests.get(next_page_url, headers=headers, params=params)
+            if response.status_code == 200:
+                issues += response.json()
+
+        # 返回获取的所有信息
+        return issues
+
+    # 如果请求失败，则返回空列表
+    else:
+        return []
+
+
+def save(all_data, data_type):
+    # 将所有信息保存到文件中
+    if data_type == "issues":
+        with open("data/issues.json", "a", encoding="utf-8") as file:
+            file.write(json.dumps(all_data, indent=4))
+    elif data_type == "commits":
+        with open("data/commits.json", "a", encoding="utf-8") as file:
+            file.write(json.dumps(all_data, indent=4))
+    else:
+        print("数据类型错误！")
+
+
+repo_owner = "pandas-dev"  # 用户名
+repo_name = "pandas"  # 仓库名
+data_type = "commits"  # issues / commits
+all_data = get_data(repo_owner, repo_name, data_type)
+save(all_data, data_type)
+print("获取{}条data！".format(len(all_data)))
